44381c0872bb9c587a83872df4bd1e14c864e455
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:37:37 -0300

    feat: adding user entity

---
+++ src/back-end/PicPaySimplificado.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using PicPaySimplificado.Domain.Enums;
+
+namespace PicPaySimplificado.Domain.Entities
+{
+    public class User : EntityBase
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public decimal Balance { get; set; } = 0;
+        public TypeUser TypeUser { get; set; } = TypeUser.Common;
+        public Guid UserIdentifier { get; set; } = Guid.NewGuid();
+    }
+}
