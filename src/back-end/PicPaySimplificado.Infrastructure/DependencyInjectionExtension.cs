d0941f8648a7d45964cd53044295a23a9a6554b7
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:39:19 -0300

    feat: adding dependency injection extension in infrastructure

---
+++ src/back-end/PicPaySimplificado.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PicPaySimplificado.Infrastructure.DataAccess;
+
+namespace PicPaySimplificado.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext_MySql(services, configuration);
+        }
+
+        private static void AddDbContext_MySql(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnectionMySql");
+
+            services.AddDbContext<PicPaySimplificadoDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        }
+    }
+}
