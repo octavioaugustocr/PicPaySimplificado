019a9a05cd4d4054362860363c2bab2986cf46a1
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:38:25 -0300

    feat: adding db context

---
+++ src/back-end/PicPaySimplificado.Infrastructure/DataAccess/PicPaySimplificadoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicPaySimplificado.Domain.Entities;
+
+namespace PicPaySimplificado.Infrastructure.DataAccess
+{
+    public class PicPaySimplificadoDbContext : DbContext
+    {
+        public PicPaySimplificadoDbContext(DbContextOptions<PicPaySimplificadoDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(options =>
+            {
+                options.Property(u => u.FullName).IsRequired().HasMaxLength(255).IsUnicode();
+                options.Property(u => u.Document).IsRequired().HasMaxLength(14).IsUnicode();
+                options.Property(u => u.Email).IsRequired().HasMaxLength(255).IsUnicode();
+                options.Property(u => u.Password).IsRequired().HasMaxLength(255);
+                options.Property(u => u.Balance).HasColumnType("decimal(18,2)").IsRequired();
+            });
+        }
+    }
+}
