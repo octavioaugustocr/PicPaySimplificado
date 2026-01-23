5c8d0f5f2270b71a78526b0e6be5a3917e4d7804
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:44:01 -0300

    feat: adding transfer entity

---
+++ src/back-end/PicPaySimplificado.Domain/Entities/Transfer.cs
@@ -0,0 +1,9 @@
+﻿namespace PicPaySimplificado.Domain.Entities
+{
+    public class Transfer : EntityBase
+    {
+        public decimal Value {  get; set; }
+        public int PayerId { get; set; }
+        public int PayeeId { get; set; }
+    }
+}
