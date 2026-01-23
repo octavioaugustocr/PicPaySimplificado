560f8d8550afb9fd14793f4f75740c004b7f10dd
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 13:09:55 -0300

    feat: adding bcrypt password encryptor

--- src/back-end/PicPaySimplificado.Infrastructure/DependencyInjectionExtension.cs
+++ src/back-end/PicPaySimplificado.Infrastructure/DependencyInjectionExtension.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PicPaySimplificado.Domain.Security.Cryptography;
 using PicPaySimplificado.Infrastructure.DataAccess;
+using PicPaySimplificado.Infrastructure.Security.Cryptography;
 
 namespace PicPaySimplificado.Infrastructure
 {
@@ -10,6 +12,7 @@ namespace PicPaySimplificado.Infrastructure
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             AddDbContext_MySql(services, configuration);
+            AddPasswordEncryptor(services);
         }
 
         private static void AddDbContext_MySql(IServiceCollection services, IConfiguration configuration)
@@ -17,6 +20,11 @@ namespace PicPaySimplificado.Infrastructure
             var connectionString = configuration.GetConnectionString("DefaultConnectionMySql");
 
             services.AddDbContext<PicPaySimplificadoDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        }
+
+        private static void AddPasswordEncryptor(IServiceCollection services)
+        {
+            services.AddScoped<IPasswordEncryptor, BCryptNet>();
         }
     }
 }
