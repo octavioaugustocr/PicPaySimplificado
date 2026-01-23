75c21e0c296fa351e95eddd965b563081ba098a0
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:43:51 -0300

    feat: adding deposit entity

---
+++ src/back-end/PicPaySimplificado.Domain/Entities/Deposit.cs
@@ -0,0 +1,8 @@
+﻿namespace PicPaySimplificado.Domain.Entities
+{
+    public class Deposit : EntityBase
+    {
+        public decimal Value { get; set; }
+        public int UserId { get; set; }
+    }
+}
