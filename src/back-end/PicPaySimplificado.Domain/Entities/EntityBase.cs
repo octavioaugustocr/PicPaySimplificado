bbc3a60d2e98a726fd513788b0160c4e581bd5be
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-23 12:36:07 -0300

    feat: adding entity base

---
+++ src/back-end/PicPaySimplificado.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace PicPaySimplificado.Domain.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedOn {  get; set; } = DateTime.Now;
+    }
+}
