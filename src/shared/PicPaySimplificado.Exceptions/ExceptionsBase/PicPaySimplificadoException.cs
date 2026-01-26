03852c0b4601fbda55f1ef9405f2dca19aa2d18e
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-25 22:49:34 -0300

    feat: adding picpay simplificado exception

---
+++ src/shared/PicPaySimplificado.Exceptions/ExceptionsBase/PicPaySimplificadoException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace PicPaySimplificado.Exceptions.ExceptionsBase
+{
+    public abstract class PicPaySimplificadoException : SystemException
+    {
+        public PicPaySimplificadoException(string message) : base(message) { }
+
+        public abstract IList<string> GetErrorMessages();
+        public abstract HttpStatusCode GetStatusCode();
+    }
+}
