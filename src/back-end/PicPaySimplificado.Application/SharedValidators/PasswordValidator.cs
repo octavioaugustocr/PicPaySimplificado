845a9364af22e0ed7e3ec8552c90694769813c92
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-27 20:47:03 -0300

    feat: adding password validator

---
+++ src/back-end/PicPaySimplificado.Application/SharedValidators/PasswordValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using PicPaySimplificado.Exceptions;
+
+namespace PicPaySimplificado.Application.SharedValidators
+{
+    public class PasswordValidator<T> : PropertyValidator<T, string>
+    {
+        public override string Name => "PasswordValidator";
+
+        public override bool IsValid(ValidationContext<T> context, string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                context.MessageFormatter.AppendArgument("ErrorMessage", ResourceMessagesException.PASSWORD_EMPTY);
+
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                context.MessageFormatter.AppendArgument("ErrorMessage", ResourceMessagesException.INVALID_PASSWORD);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override string GetDefaultMessageTemplate(string errorCode) => "{ErrorMessage}";
+    }
+}
