4a8d8531bb24cf8e1a3dbb9c62eed18203e211f9
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-28 19:55:47 -0300

    feat: adding response error json

---
+++ src/shared/PicPaySimplificado.Communication/Responses/Error/ResponseErrorJson.cs
@@ -0,0 +1,18 @@
+﻿namespace PicPaySimplificado.Communication.Responses.Error
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+        public bool TokenIsExpired { get; set; }
+
+        public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+        public ResponseErrorJson(string error)
+        {
+            Errors = new List<string>
+            {
+                error
+            };
+        }
+    }
+}
