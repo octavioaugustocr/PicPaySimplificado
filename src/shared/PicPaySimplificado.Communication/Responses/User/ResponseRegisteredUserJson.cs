469d505082c2721ea282fbf18f9b1efbec0f6269
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-01-30 23:25:57 -0300

    feat: adding response registered user json

---
+++ src/shared/PicPaySimplificado.Communication/Responses/User/ResponseRegisteredUserJson.cs
@@ -0,0 +1,10 @@
+﻿namespace PicPaySimplificado.Communication.Responses.User
+{
+    public class ResponseRegisteredUserJson
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+    }
+}
