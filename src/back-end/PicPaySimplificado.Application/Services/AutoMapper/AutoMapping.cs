9ba0e8d09220ac08bae56f23c6e9469c02798b80
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2026-02-22 20:34:14 -0300

    feat: adding auto mapper of request to domain

---
+++ src/back-end/PicPaySimplificado.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PicPaySimplificado.Communication.Requests.User.Register;
+using PicPaySimplificado.Domain.Entities;
+using PicPaySimplificado.Domain.Enums;
+
+namespace PicPaySimplificado.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToDomain();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<RequestRegisterUserCommonJson, User>()
+                .ForMember(userEntity => userEntity.Password, option => option.Ignore())
+                .ForMember(userEntity => userEntity.Document, option => option.MapFrom(request => request.Cpf));
+
+            CreateMap<RequestRegisterUserMerchantJson, User>()
+                .ForMember(userEntity => userEntity.Password, option => option.Ignore())
+                .ForMember(userEntity => userEntity.Document, option => option.MapFrom(request => request.Cnpj))
+                .ForMember(userEntity => userEntity.TypeUser, option => option.MapFrom(request => TypeUser.Merchant));
+        }
+    }
+}
